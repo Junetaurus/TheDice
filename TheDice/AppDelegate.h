78ff4d934ca865ec9188cc7316c4cd4de3233186
Taurus-GitHub <zj0376@163.com>
2017-06-01 14:29:29 +0800

    Initial Commit

---
+++ TheDice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheDice
+//
+//  Created by ZhangJun on 2017/6/1.
+//  Copyright © 2017年 ZhangJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheDice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheDice
+//
+//  Created by ZhangJun on 2017/6/1.
+//  Copyright © 2017年 ZhangJun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
